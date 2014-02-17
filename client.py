36983b56b28dae0698bdc1df49eac988060a71e6
Sam Jenkins <sjenkinsc@gmail.com>
2014-02-17 17:19:48 -0600

    Quaternion From To

--- client.py
+++ client.py
@@ -19,6 +19,7 @@ from panda3d.core import LVector3f, Point3
 from panda3d.core import NodePathCollection
 from panda3d.core import Quat
 from mecha01 import Planet, CharacterController
+from math import sqrt
 
 class Mecha01Client(ShowBase):
     def __init__(self):
@@ -44,13 +45,17 @@ class Mecha01Client(ShowBase):
         self.planet.perlin_terrain(5, 0.5)
         self.planet.build_node_path(render, self.world)
 
-        shape = BulletCapsuleShape(
-            self.planet.get_height_unit() / 2,
-            self.planet.get_height_unit())
+        shape = BulletCapsuleShape(0.25, 0.5)
         node = BulletRigidBodyNode('Capsule')
         node.addShape(shape)
         np = render.attachNewNode(node)
-        np.setPos(0, 1.1, 1.1)
+        np.setPos(Vec3(0, 0.8, 0.8)*self.planet.get_radius())
+        new_up_vector = Vec3(
+            np.getPos() - self.planet.get_node_path().getPos())
+        old_up_vector = Vec3(0, 0, 1)
+        q = self.__quatFromTo(old_up_vector, new_up_vector)
+        np.setQuat(q)
+    
         self.np01 = np
         self.world.attachRigidBody(node)
 
@@ -75,20 +80,30 @@ class Mecha01Client(ShowBase):
         node = self.np01.getNode(0)
         pos = self.np01.getPos()
         gravity = self.gravity(pos)
-        self.np01.setQuat(Quat(0, 1.1, 1.1, 0))
+        #self.np01.setQuat(Quat(0, 1.1, 1.1, 0))
 
         self.world.doPhysics(dt)
 
         return Task.cont
 
     def spinCameraTask(self, task):
-        angleDegrees = task.time * 6.0
-        angleRadians = angleDegrees * (pi / 180.0)
-        self.camera.setPos(
-            100.0 * sin(angleRadians),
-            -100.0 * cos(angleRadians), 0.0)
-        self.camera.setHpr(angleDegrees, 0, 0)
+        # angleDegrees = task.time * 6.0
+        # angleRadians = angleDegrees * (pi / 180.0)
+        # self.camera.setPos(
+        #     100.0 * sin(angleRadians),
+        #     -100.0 * cos(angleRadians), 0.0)
+        # self.camera.setHpr(angleDegrees, 0, 0)
+        self.camera.setPos(self.np01.getPos()*1.5)
+        self.camera.lookAt(self.np01)
         return Task.cont
+
+    def __quatFromTo(self, v0, v1):
+        print (v0, v1)
+        q = Quat(
+            sqrt(v0.length()**2 * v1.length()**2) + v0.dot(v1),
+            v0.cross(v1))
+        q.normalize()
+        return q
 
 app = Mecha01Client()
 app.run()
